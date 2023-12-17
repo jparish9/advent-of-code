b970af4fb5c78be1b5cf649b9e6d4afa8c3f8e6b
Jerad Parish <reg.ghub@jdp.sent.com>
2023-12-17 17:04:08 -0600

    use a SortedSet to speed up open list; refactorings; runs on input in ~3s now

--- 2023/Day17.cs
+++ 2023/Day17.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-
 namespace AOC.AOC2023;
 
 public class Day17 : Day<Day17.Map>
@@ -22,47 +20,70 @@ public class Day17 : Day<Day17.Map>
             public int State { get; set; }          // memory of direction of last 3 steps taken; bitmask (0-3 left, 4-15 right, 16-63 down, 64-255 up)
             public Node? Parent { get; set; }
 
-            public bool Equals(Node node)
+            // node is unique (for purposes of A* search) if position and state are the same
+            public int HashCode { get => Position.GetHashCode() ^ State.GetHashCode(); }
+        }
+
+        // for comparing F-values.  if F is equal, compare hashcodes to ensure uniqueness.
+        public class NodeComparer : IComparer<Node>
+        {
+            public int Compare(Node? x, Node? y)
             {
-                return Position == node.Position;
+                if (x == null && y == null) return 0;
+                else if (x != null && y == null) return -1;
+                else if (x == null && y != null) return 1;
+
+                var f = x!.F.CompareTo(y!.F);
+                if (f != 0) return f;
+
+                return x.HashCode.CompareTo(y.HashCode);
             }
         }
 
         /*
-            I think we need to do something like this
+            For part 2, we need to keep track of the node state (last N steps in a particular direction) in addition to the position.
+            Note that the end state is not just reaching the end node, but also the correct state of the end node (>=4 steps in the current direction).
             https://stackoverflow.com/questions/39305377/dealing-with-a-time-based-constraint-in-a-star-on-2d-grid-world
-            but instead of a time constraint, we have a straight-line constraint,
-            so we need to keep track of the last 3 steps taken and make sure we don't go more than 3 steps in any direction.
-            nodes with the same position can be revisited if the last 3 steps are different.
+            ^ Instead of a time constraint, we have a state-of-last-N-nodes constraint.  We need to treat this state as a third dimension to our search grid.
         */
-
-        public List<Node> Search()
+        public List<Node> Search(int minDirSteps, int maxDirSteps)
         {
+            var divisor = maxDirSteps+1;
+
             var startNode = new Node() { Position = (0,0) };
             var endNodes = new HashSet<(int, int)>() { (Grid[0].Length-1, Grid.Length-1) };
 
-            var open = new Dictionary<(int, int, int), Node>();  // for open we need to keep track of the positions and the order
-            var closed = new HashSet<(int, int, int)>();         // for closed we only need to keep track of the positions (and check existence, not order)
+            // maintain the open list both as a hash for O(1) lookup by position+state, and as a sorted set to quickly get the node with the lowest F score
+            var open = new Dictionary<int, Node>();
+            var openF = new SortedSet<Node>(new NodeComparer());
+
+            // for closed, we don't need the node itself, just its hash to keep track of them.
+            var closed = new HashSet<int>();
 
             var path = new List<Node>();              // to quiet null warning; empty path will be returned if no path found
 
-            open.Add((startNode.Position.Item1, startNode.Position.Item2, startNode.State), startNode);
+            open.Add(startNode.HashCode, startNode);
+            openF.Add(startNode);
 
-            var visited = 0;
+            var dirs = new List<(int, int)>() { (-1, 0), (1, 0), (0, -1), (0, 1) };
+
+            /*var visited = 0;
             var sw = new Stopwatch();
-            sw.Start();
+            sw.Start();*/
 
             while (open.Any())
             {
                 // get node with lowest F score
-                var currentNode = open.Values.OrderBy(p => p.F).First();
+                var currentNode = openF.First();
+                var currentHash = currentNode.HashCode;
 
                 // remove from open list, add to closed list
-                open.Remove((currentNode.Position.Item1, currentNode.Position.Item2, currentNode.State));
-                closed.Add((currentNode.Position.Item1, currentNode.Position.Item2, currentNode.State));
+                open.Remove(currentHash);
+                openF.Remove(currentNode);
+                closed.Add(currentHash);
 
                 // if we've reached the end, we're done
-                if (endNodes.Contains(currentNode.Position) && currentNode.State % 11 >= 4)
+                if (endNodes.Contains(currentNode.Position) && currentNode.State % divisor >= minDirSteps)
                 {
                     var node = currentNode;
                     while (node != null)
@@ -83,26 +104,16 @@ public class Day17 : Day<Day17.Map>
 
                 // get all adjacent nodes
                 var adjacentNodes = new List<Node>();
-                var dirs = new List<(int, int)>() { (-1, 0), (1, 0), (0, -1), (0, 1) };
 
                 var x = currentNode.Position.Item1;
                 var y = currentNode.Position.Item2;
-
-                /*System.Console.Write($"Finding neighbors of {x},{y}, state " + (currentNode.State/11));
-                var test = currentNode.Parent;
-                for (var i=1; i<=5 && test != null; i++)
-                {
-                    System.Console.Write($" -> {test.Position.Item1},{test.Position.Item2}");
-                    test = test.Parent;
-                }
-                System.Console.WriteLine();*/
 
                 // find neighbors
                 // for part 2, once we start going in a particular direction, we must continue in that direction for at least 4 and no more than 10 steps.
                 // we can determine if a turn is allowed without considering each of the 4 directions.
                 // if a turn is not allowed, then we don't need to loop over all 4 directions.
                 // conveniently, the saved state helps a lot here.
-                int? onlyDirAllowed = currentNode.Equals(startNode) || currentNode.State % 11 >= 4 ? null : currentNode.State / 11;
+                int? onlyDirAllowed = currentNode.Position == startNode.Position || currentNode.State % divisor >= minDirSteps ? null : currentNode.State / divisor;
 
                 for (var dirIndex=0; dirIndex<4; dirIndex++)
                 {
@@ -116,54 +127,44 @@ public class Day17 : Day<Day17.Map>
                     if (newY == startNode.Position.Item2 && newX == startNode.Position.Item1) continue;            // don't revisit start
                     if (newX == currentNode.Parent?.Position.Item1 && newY == currentNode.Parent?.Position.Item2) continue;    // don't turn around
 
-                    var stepsInThisDir = 0;
-
                     // for part 2, once we start going in a particular direction, we must continue in that direction for at least 4 and no more than 10 steps.
                     // to check for "at least 4", we need to check the last 3 steps (up to currentNode, ) and make sure they are all in the same direction.
 
                     // check straight-line constraint
-                    var checkNode = currentNode;
-                    while (checkNode != null && stepsInThisDir <= 10)
-                    {
-                        if (checkNode.Position.Item1 != newX-dir.Item1*(stepsInThisDir+1) || checkNode.Position.Item2 != newY-dir.Item2*(stepsInThisDir+1))
-                        {
-                            break;
-                        }
-
-                        stepsInThisDir++;
-                        checkNode = checkNode.Parent;
-                    }
-
-                    if (stepsInThisDir > 10) continue;
+                    var stepsInThisDir = currentNode.State / divisor == dirIndex ? currentNode.State % divisor : 0;
+                    if (stepsInThisDir >= maxDirSteps) continue;
 
                     // allowable neighbor
-                    //System.Console.WriteLine("  adding neighbor " + newX + "," + newY + " state " + (11*dirIndex + stepsInThisDir));
-                    var newNode = new Node() { Position = (newX, newY), Parent = currentNode, State = currentNode.State = 11*dirIndex + stepsInThisDir};
+                    var newNode = new Node() { Position = (newX, newY), Parent = currentNode, State = divisor*dirIndex+stepsInThisDir+1};
                     adjacentNodes.Add(newNode);
                 }
 
                 // loop through adjacent nodes
                 foreach (var adjacentNode in adjacentNodes)
                 {
-                    if (closed.Contains((adjacentNode.Position.Item1, adjacentNode.Position.Item2, adjacentNode.State))) continue;
+                    var hash = adjacentNode.HashCode;
+                    if (closed.Contains(hash)) continue;
 
                     adjacentNode.G = currentNode!.G + Grid[adjacentNode.Position.Item2][adjacentNode.Position.Item1];
                     // H (heuristic cost) is manhattan distance to end.  if multiple ends, use the closest one.
                     adjacentNode.H = endNodes.Min(p => Math.Abs(adjacentNode.Position.Item1 - p.Item1) + Math.Abs(adjacentNode.Position.Item2 - p.Item2));
                     adjacentNode.F = adjacentNode.G + adjacentNode.H;
 
-                    if (open.ContainsKey((adjacentNode.Position.Item1, adjacentNode.Position.Item2, adjacentNode.State))
-                        && open[(adjacentNode.Position.Item1, adjacentNode.Position.Item2, adjacentNode.State)].G <= adjacentNode.G) continue;
+                    if (open.ContainsKey(hash) && open[hash].G <= adjacentNode.G) continue;         // already in open list with lower G score
 
-                    open.Add((adjacentNode.Position.Item1, adjacentNode.Position.Item2, adjacentNode.State), adjacentNode);
+                    open.Add(hash, adjacentNode);
+                    openF.Add(adjacentNode);
                 }
 
+                /*
+                // benchmark
                 visited++;
                 if (visited % 10000 == 0)
                 {
                     System.Console.WriteLine("visited " + visited + " nodes, open list size " + open.Count + ", current node " + currentNode.Position + " state " + (currentNode.State/11) + " in " + sw.ElapsedMilliseconds + "ms");
-                    sw.Restart();
+1                    sw.Restart();
                 }
+                */
             }
 
             Console.WriteLine("did not find a path!");
@@ -171,17 +172,14 @@ public class Day17 : Day<Day17.Map>
         }
     }
 
-        // node class for A* search
-    
-
     protected override long Part1()
     {
-        return Input.Search().Last().G;
+        return Input.Search(0, 3).Last().G;
     }
 
     protected override long Part2()
     {
-        throw new NotImplementedException();
+        return Input.Search(4, 10).Last().G;
     }
 
     protected override Map Parse(string input)
