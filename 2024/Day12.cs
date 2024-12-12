779021e4fdae8d7f0c8ee5d4718b75aeb8b5ada9
Jerad Parish <reg.ghub@jdp.sent.com>
2024-12-12 11:24:09 -0600

    much cleanup, very wow (day 12)

--- 2024/Day12.cs
+++ 2024/Day12.cs
@@ -6,9 +6,40 @@ public class Day12 : Day<Day12.Garden>
 {
     protected override string? SampleRawInput { get => "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE"; }
 
+    private static readonly List<(int x, int y)> Directions = new() { (0, 1), (-1, 0), (0, -1), (1, 0) };            // down, left, up, right
+
+    public class Plot
+    {
+        public char Type;
+        public int? Region;
+        public int Y;
+        public int X;
+        public bool[] CountPerimeters = new bool[4] { true, true, true, true };         // down, left, up, right
+        public int[] PerimeterSides = new int[4] { -1, -1, -1, -1 };                    // for part 2
+    }
+
     public class Garden
     {
         public required Plot[][] Map;
+
+        public bool InBounds(int x, int y)
+        {
+            return x >= 0 && x < Map[0].Length && y >= 0 && y < Map.Length;
+        }
+
+        public void FindRegions()
+        {
+            var nextRegion = 1;
+            for (int i = 0; i < Map.Length; i++)
+            {
+                for (int j = 0; j < Map[i].Length; j++)
+                {
+                    // if this plot doesn't yet have a region yet, start it and flood fill.
+                    if (Map[i][j].Region != null) continue;
+                    FloodFill(i, j, nextRegion++);
+                }
+            }
+        }
 
         public long Price()
         {
@@ -32,7 +63,6 @@ public class Day12 : Day<Day12.Garden>
             var price = 0;
             var regions = Map.SelectMany(p => p).Where(p => p.Region != null).Select(p => (int)p.Region!).Distinct();
 
-
             foreach (var region in regions)
             {
                 var plots = Map.SelectMany(p => p).Where(p => p.Region == region);
@@ -41,65 +71,26 @@ public class Day12 : Day<Day12.Garden>
                 var side = 1;
                 foreach (var plot in plots)
                 {
-                    if (plot.CountPerimeters[0] && plot.PerimeterSides[0] == -1)
+                    for (var k=0; k<Directions.Count; k++)
                     {
-                        plot.PerimeterSides[0] = side;
-                        if (plot.X > 0)
+                        var walks = new List<(int x, int y)> { Directions[(k+1)%4], Directions[(k+3)%4] };          // walk left and right if on bottom edge, etc.
+                        if (plot.CountPerimeters[k] && plot.PerimeterSides[k] == -1)
                         {
-                            WalkSide(Map[plot.Y][plot.X-1], -1, 0, side, 0, region);
+                            plot.PerimeterSides[k] = side;
+                            foreach (var walk in walks)
+                            {
+                                var (x, y) = walk;
+                                if (InBounds(plot.X + x, plot.Y + y))
+                                {
+                                    WalkSide(Map[plot.Y + y][plot.X + x], x, y, side, k, region);
+                                }
+                            }
+                            side++;
                         }
-                        if (plot.X < Map[plot.Y].Length - 1)
-                        {
-                            WalkSide(Map[plot.Y][plot.X+1], 1, 0, side, 0, region);
-                        }
-                        side++;
-                    }
-
-                    if (plot.CountPerimeters[1] && plot.PerimeterSides[1] == -1)
-                    {
-                        plot.PerimeterSides[1] = side;
-                        if (plot.Y > 0)
-                        {
-                            WalkSide(Map[plot.Y-1][plot.X], 0, -1, side, 1, region);
-                        }
-                        if (plot.Y < Map.Length - 1)
-                        {
-                            WalkSide(Map[plot.Y+1][plot.X], 0, 1, side, 1, region);
-                        }
-                        side++;
-                    }
-
-                    if (plot.CountPerimeters[2] && plot.PerimeterSides[2] == -1)
-                    {
-                        plot.PerimeterSides[2] = side;
-                        if (plot.X > 0)
-                        {
-                            WalkSide(Map[plot.Y][plot.X-1], -1, 0, side, 2, region);
-                        }
-                        if (plot.X < Map[plot.Y].Length - 1)
-                        {
-                            WalkSide(Map[plot.Y][plot.X+1], 1, 0, side, 2, region);
-                        }
-                        side++;
-                    }
-
-                    if (plot.CountPerimeters[3] && plot.PerimeterSides[3] == -1)
-                    {
-                        plot.PerimeterSides[3] = side;
-                        if (plot.Y > 0)
-                        {
-                            WalkSide(Map[plot.Y-1][plot.X], 0, -1, side, 3, region);
-                        }
-                        if (plot.Y < Map.Length - 1)
-                        {
-                            WalkSide(Map[plot.Y+1][plot.X], 0, 1, side, 3, region);
-                        }
-                        side++;
                     }
                 }
 
-                var perimeter = plots.SelectMany(p => p.PerimeterSides).Select(p => p).Where(p => p != -1).Distinct().Count();
-
+                var perimeter = plots.SelectMany(p => p.PerimeterSides).Select(p => p).Where(p => p != -1).Distinct().Count();      // count sides
                 var area = plots.Count();
 
                 price += perimeter*area;
@@ -108,98 +99,56 @@ public class Day12 : Day<Day12.Garden>
             return price;
         }
 
+        // DFS flood fill, given a current position and a region id
+        private void FloodFill(int i, int j, int region)
+        {
+            if (Map[i][j].Region != null) return;
+
+            Map[i][j].Region = region;
+
+            // check for connected Region in every direction to nullify mutual perimiters
+            for (var k=0; k<Directions.Count; k++)
+            {
+                var (x, y) = Directions[k];
+                if (InBounds(j + x, i + y) && Map[i + y][j + x].Region == region)
+                {
+                    Map[i][j].CountPerimeters[k] = false;
+                    Map[i + y][j + x].CountPerimeters[(k+2)%4] = false;           // opposite direction
+                }
+            }
+
+            foreach (var (x, y) in Directions)
+            {
+                if (InBounds(j + x, i + y) && Map[i + y][j + x].Type == Map[i][j].Type)
+                {
+                    FloodFill(i + y, j + x, region);
+                }
+            }
+        }
+
         // walk a perimeter "side" in one direction until it ends, marking those edges as part of the same side
-        private void WalkSide(Plot p, int xDir, int yDir, int side, int perimiterSide, int region)
+        private void WalkSide(Plot p, int xDir, int yDir, int side, int perimiterDirection, int region)
         {
             if (p.Region != region) return;
-            if (!p.CountPerimeters[perimiterSide] || p.PerimeterSides[perimiterSide] != -1) return;
+            if (!p.CountPerimeters[perimiterDirection] || p.PerimeterSides[perimiterDirection] != -1) return;
 
-            p.PerimeterSides[perimiterSide] = side;
+            p.PerimeterSides[perimiterDirection] = side;
 
-            if (xDir != 0 && p.X+xDir >= 0 && p.X+xDir < Map[0].Length)
-                WalkSide(Map[p.Y][p.X+xDir], xDir, 0, side, perimiterSide, region);
-            else if (yDir != 0 && p.Y+yDir >= 0 && p.Y+yDir < Map.Length)
-                WalkSide(Map[p.Y+yDir][p.X], 0, yDir, side, perimiterSide, region);
+            if (!InBounds(p.X + xDir, p.Y + yDir)) return;
+
+            WalkSide(Map[p.Y+yDir][p.X+xDir], xDir, yDir, side, perimiterDirection, region);
         }
-    }
-
-    public class Plot
-    {
-        public char Type;
-        public int? Region;
-        public int Y;
-        public int X;
-        public bool[] CountPerimeters = new bool[4] { true, true, true, true };         // down, left, up, right
-        public int[] PerimeterSides = new int[4] { -1, -1, -1, -1 };                    // for part 2
     }
 
     protected override long Part1()
     {
-        var nextRegion = 1;
-        for (int i = 0; i < Input.Map.Length; i++)
-        {
-            for (int j = 0; j < Input.Map[i].Length; j++)
-            {
-                // if this plot doesn't yet have a region yet, start it and flood fill.
-                if (Input.Map[i][j].Region != null) continue;
-
-                FloodFill(i, j, nextRegion++);
-            }
-        }
-
+        Input.FindRegions();
         return Input.Price();
     }
 
     protected override long Part2()
     {
         return Input.BulkPrice();
-    }
-
-
-    private void FloodFill(int i, int j, int region)
-    {
-        if (Input.Map[i][j].Region != null) return;
-
-        Input.Map[i][j].Region = region;
-
-        // check for connected Region in every direction to nullify perimiter
-        if (i > 0 && Input.Map[i - 1][j].Region == region)
-        {
-            Input.Map[i][j].CountPerimeters[0] = false;
-            Input.Map[i - 1][j].CountPerimeters[2] = false;
-        }
-        if (j > 0 && Input.Map[i][j - 1].Region == region)
-        {
-            Input.Map[i][j].CountPerimeters[3] = false;
-            Input.Map[i][j - 1].CountPerimeters[1] = false;
-        }
-        if (i < Input.Map.Length - 1 && Input.Map[i + 1][j].Region == region)
-        {
-            Input.Map[i][j].CountPerimeters[2] = false;
-            Input.Map[i + 1][j].CountPerimeters[0] = false;
-        }
-        if (j < Input.Map[i].Length - 1 && Input.Map[i][j + 1].Region == region)
-        {
-            Input.Map[i][j].CountPerimeters[1] = false;
-            Input.Map[i][j + 1].CountPerimeters[3] = false;
-        }
-
-        if (i > 0 && Input.Map[i - 1][j].Type == Input.Map[i][j].Type)
-        {
-            FloodFill(i - 1, j, region);
-        }
-        if (j > 0 && Input.Map[i][j - 1].Type == Input.Map[i][j].Type)
-        {
-            FloodFill(i, j - 1, region);
-        }
-        if (i < Input.Map.Length - 1 && Input.Map[i + 1][j].Type == Input.Map[i][j].Type)
-        {
-            FloodFill(i + 1, j, region);
-        }
-        if (j < Input.Map[i].Length - 1 && Input.Map[i][j + 1].Type == Input.Map[i][j].Type)
-        {
-            FloodFill(i, j + 1, region);
-        }
     }
 
     protected override Garden Parse(string input)
